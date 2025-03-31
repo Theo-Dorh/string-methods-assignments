e7a79381ee7cc56b67d47ce18832ba513009bce4
Theo-Dorh <dorhtheophilus@gmail.com>
2025-03-31 16:37:09 +0000

    Included quotation marks for the function output

--- strgLength.js
+++ strgLength.js
@@ -13,7 +13,7 @@ function strgLength () {
 
     let result = document.createElement("div");
     
-    result.textContent = `The length of your string is ${length}`;
+    result.textContent = `The length of your string is "${length}"`;
     
     result.style.cssText = "padding: 10px 20px; border-radius: 50px; background-color: #77c0dd; color: black; display: inline-block; margin-top: 10px;";
 
